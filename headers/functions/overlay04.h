a79e260cfd1460c46b6296d16dc82f4025dec430
Parker Zhiss <86489014+HappyLappy1@users.noreply.github.com>
2026-05-02 01:01:26 -0400

    overlay 02 functions

--- headers/functions/overlay04.h
+++ headers/functions/overlay04.h
@@ -2,5 +2,5 @@
 #define HEADERS_FUNCTIONS_OVERLAY04_H_
 
 struct c_letter_scene * CLetterSceneConstructor(struct c_letter_scene *this,struct megastruct *param_2);
-void struct ret_0211d560(void);
+void ret_0211d560(void);
 #endif
